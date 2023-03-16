cf152a1b2a4c0fb675c8ae950357405fe37dd768
Dustin Dixon <bobdeus@gmail.com>
2023-03-16 15:15:49 -0600

    add NumberLineJumps

---
+++ HackerRankStuff/Code/NumberLineJumps.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRankStuff.Code;
+
+public class NumberLineJumps
+{
+    public static string Kangaroo(int x1, int v1, int x2, int v2)
+    {
+        var firstHops = GetAllHops(x1, v1);
+        var secondHops = GetAllHops(x2, v2);
+        
+        for (var i = 0; i < firstHops.Count; i++)
+        {
+            if (firstHops[i] == secondHops[i])
+            {
+                return "YES";
+            }
+        }
+        
+        return "NO";
+    }
+
+    private static List<int> GetAllHops(int start, int distance)
+    {
+        return Enumerable.Range(start, 10_000).Aggregate(new List<int>{start}, (accumulator, _) =>
+        {
+            accumulator.Add(accumulator.Last() + distance);
+            return accumulator;
+        });
+    }
+}
---
+++ HackerRankStuffTests/NumberLineJumpsTests.cs
@@ -0,0 +1,16 @@
+﻿using HackerRankStuff.Code;
+
+namespace HackerRankStuffTests;
+
+public class NumberLineJumpsTests
+{
+    [Test]
+    public void TestKangaroo()
+    {
+        Assert.That(NumberLineJumps.Kangaroo(0, 3, 4, 2), Is.EqualTo("YES"));
+        Assert.That(NumberLineJumps.Kangaroo(0, 2, 5, 3), Is.EqualTo("NO"));
+        Assert.That(NumberLineJumps.Kangaroo(0, 6, 18, 3), Is.EqualTo("YES"));
+        Assert.That(NumberLineJumps.Kangaroo(0, 2, 1, 1), Is.EqualTo("YES"));
+    }
+}
+
